59405fa1188907c365de4262cb21dd6d9909434e
Mutyala Harshith <harshithxdev@gmail.com>
2025-05-07 16:26:11 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+from pyrogram import Client
+from aiohttp import web
+from main.web_support import web_server
+from config import *
+import os
+
+
+class Bot(Client):    
+
+    def __init__(self):
+        super().__init__(
+            name="MeshRename",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "main"},
+            sleep_threshold=10,
+        )
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()        
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+        
+                
+        
+    async def stop(self, *args):
+       await super().stop()      
+       print("Bot Restarting........")
+
+bot = Bot()
+bot.run()
